b8922a9b7be4c7e76c4d9ea7505677f0b355b935
ryohassay <43590790+ryohassay@users.noreply.github.com>
2022-06-03 17:42:27 +0900

    Work on "Alternating Characters" problem

---
+++ string-manipulation/alternating_characters.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    size = len(s)
+    count = 0
+    char = s[0]
+    for i in range(1, size):
+        if s[i] == char:
+            count += 1
+        else:
+            char = s[i]
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
